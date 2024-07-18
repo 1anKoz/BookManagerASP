7965dde26d90036acaba913f8dc5a6612fe44174
1anKoz <jankozinski@wp.pl>
2024-07-18 17:25:14 +0200

    Creating simplified models with their props

---
+++ BookManagerASP/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookManagerASP.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        //public string Description { get; set; }
+        public string Author { get; set; }
+        //public string CoverUrl { get; set; }
+        //public int Isbn { get; set; }
+    }
+}
---
+++ BookManagerASP/Models/Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace BookManagerASP.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int? Page {  get; set; }
+        public bool IsFavourite { get; set; }
+    }
+}
