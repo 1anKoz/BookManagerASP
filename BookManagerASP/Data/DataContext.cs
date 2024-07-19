d2351c8104987b337b848674086556c05ee5680b
1anKoz <jankozinski@wp.pl>
2024-07-19 02:18:25 +0200

    Start adding Context

---
+++ BookManagerASP/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using BookManagerASP.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace BookManagerASP.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookPrivate> BooksPrivates { get; set; }
+        public DbSet<UserEntity> UserEntities { get; set; }
+        public DbSet<Shelf> Shelves { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<>
+        }
+    }
+}
