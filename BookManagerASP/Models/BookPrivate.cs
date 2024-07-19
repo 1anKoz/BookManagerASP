0d6e42d11ed398f008697d24cc10fef688c6083c
1anKoz <jankozinski@wp.pl>
2024-07-19 02:01:05 +0200

    Create relationships (test) and add new db to ms sql and add its connection string in appsettings.json

---
+++ BookManagerASP/Models/BookPrivate.cs
@@ -0,0 +1,16 @@
+﻿namespace BookManagerASP.Models
+{
+    public class BookPrivate
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public ICollection<Quote>? Quotes { get; set; }
+        public bool IsFavourite { get; set; }
+
+        public virtual Book Book { get; set; }
+        public int BookId { get; set; }
+
+        public int UserEntityId { get; set; }
+        public UserEntity UserEntity { get; set; }
+    }
+}
--- BookManagerASP/Models/UserEntity.cs
+++ BookManagerASP/Models/UserEntity.cs
@@ -9,5 +9,9 @@
         public string NickName { get; set; }
         public string Password { get; set; }
         //public string? PhotoUrl { get; set; }
+
+        public virtual ICollection<Shelf> Shelves { get; set; }
+
+        public virtual ICollection<BookPrivate> BookPrivates { get; set; }
     }
 }
