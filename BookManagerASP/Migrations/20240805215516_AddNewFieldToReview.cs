ef2ccdf88e735700cecab2277ec968e109e77487
1anKoz <jankozinski@wp.pl>
2024-08-05 23:56:35 +0200

    Add new field (Rating) to the Review model

---
+++ BookManagerASP/Migrations/20240805215516_AddNewFieldToReview.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookManagerASP.Migrations
+{
+    public partial class AddNewFieldToReview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Review",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Review");
+        }
+    }
+}
