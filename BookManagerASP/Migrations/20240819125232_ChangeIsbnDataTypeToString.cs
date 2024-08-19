51a21c9eb8d74f60ff31235a33734e33dffe8cae
1anKoz <jankozinski@wp.pl>
2024-08-19 14:58:02 +0200

    Change data type of Isbn property in the Book model

---
+++ BookManagerASP/Migrations/20240819125232_ChangeIsbnDataTypeToString.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookManagerASP.Migrations
+{
+    public partial class ChangeIsbnDataTypeToString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Isbn",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Isbn",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
